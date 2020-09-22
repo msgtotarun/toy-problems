3c4c4f236510c237eb638ea8bbb2ad594214aafd
tarun <msgtotarun@gmail.com>
2020-09-22 19:26:49 +0530

    added methods-get,put,getcache

--- lru.py
+++ lru.py
@@ -4,8 +4,34 @@ import socket
 class lru:
 
     def __init__(self, size):
-        pass
+        self.size = size
+        self.lru = []
+        self.lruCache = {}
     def get(self, key, default=None):
-        pass
+        if key in self.lru:
+            return self.lruCache[key]
+        else:
+            return default
     def put(self, key):
-        pass
+        if(len(self.lru) < self.size):
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)
+                hostname = "www."+str(key)+".com"
+                self.lruCache[key] = socket.gethostbyname(hostname)
+            else:
+                self.lru.append(key)
+                hostname = "www."+str(key)+".com"
+                self.lruCache[key] = socket.gethostbyname(hostname)
+        else:
+
+            var = self.lru.pop(0)
+            self.lru.append(key)
+            del self.lruCache[var]
+            hostname = "www."+str(key)+".com"
+            self.lruCache[key] = socket.gethostbyname(hostname)
+    def get_cache(self):
+        ls = []
+        for key in reversed(self.lru):
+            ls.append(key+"-" + self.lruCache[key])
+        return ls
